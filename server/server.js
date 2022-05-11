72adf8cb0431295031b2f7d979b92ad92fe158fb
Vailorium <casterthe5587@gmail.com>
2022-05-11 13:55:38 +1200

    init bootstrap

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+// http://www.gcmap.com/map?P=YSSY+-NZNE&MS=wls&MR=120&MX=720x360&PM=*
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
