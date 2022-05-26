3922f97339c55896e29bc3d919d1fe9735a4684b
Vailorium <casterthe5587@gmail.com>
2022-05-26 12:44:17 +1200

    add routes table, adjust bookings and flights table

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -18,12 +18,9 @@ CREATE TABLE IF NOT EXISTS planes (
   CONSTRAINT pk_plane PRIMARY KEY (id)
 );
 
+/* handles main flights, flights can have multiple routes */
 CREATE TABLE IF NOT EXISTS flights (
   id INT GENERATED ALWAYS AS IDENTITY,
-  flight_route VARCHAR(100) NOT NULL,
-  departure_time TIMESTAMP NOT NULL,
-  arrival_time TIMESTAMP NOT NULL,
-  price INT NOT NULL,
   plane_id INT NOT NULL,
   CONSTRAINT pk_flight PRIMARY KEY (id),
   CONSTRAINT fk_flights_planes
@@ -31,15 +28,30 @@ CREATE TABLE IF NOT EXISTS flights (
       REFERENCES planes(id)
 );
 
+/* search for available seats based on plane id */
+CREATE TABLE IF NOT EXISTS routes (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  flight_id INT NOT NULL,
+  departure_location VARCHAR(50) NOT NULL,
+  arrival_location VARCHAR(50) NOT NULL,
+  departure_time TIMESTAMP NOT NULL,
+  arrival_time TIMESTAMP NOT NULL,
+  price INT NOT NULL,
+  CONSTRAINT pk_route PRIMARY KEY (id),
+  CONSTRAINT fk_routes_flights
+    FOREIGN KEY (flight_id)
+      REFERENCES flights(id)
+);
+
 CREATE TABLE IF NOT EXISTS bookings (
   id INT GENERATED ALWAYS AS IDENTITY,
   user_id INT NOT NULL,
-  flight_id INT NOT NULL,
+  route_id INT NOT NULL,
   CONSTRAINT pk_booking PRIMARY KEY (id),
   CONSTRAINT fk_bookings_users
     FOREIGN KEY (user_id)
       REFERENCES users(id),
-  CONSTRAINT fk_bookings_flights
-    FOREIGN KEY (flight_id)
-      REFERENCES flights(id)
+  CONSTRAINT fk_bookings_routes
+    FOREIGN KEY (route_id)
+      REFERENCES routes(id)
 );
