f385aff972291a649ffb2dff95073de74122a002
Vailorium <casterthe5587@gmail.com>
2022-05-27 13:57:46 +1200

    add pre-set table data, change password and salt to varchar

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
   id INT GENERATED ALWAYS AS IDENTITY,
   display_name VARCHAR(50) NOT NULL,
   username VARCHAR(50) UNIQUE NOT NULL,
-  password BYTEA NOT NULL,
-  salt BYTEA NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  salt VARCHAR(100) NOT NULL,
   CONSTRAINT pk_user PRIMARY KEY (id)
 );
 
@@ -55,3 +55,30 @@ CREATE TABLE IF NOT EXISTS bookings (
     FOREIGN KEY (route_id)
       REFERENCES routes(id)
 );
+
+DO
+$do$
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM users) THEN
+    /* username: test, password: asdfasdf */
+    INSERT INTO users (display_name, username, password, salt)
+    VALUES ('test', 'test', '8a333b2c806cb87fd21cd3db8bbb1746c1c2e400f72b1dfcae6b4ef84a32ee16', 'f94304f12e335d3e7e399538e947eab1');
+  END IF;
+
+  IF NOT EXISTS (SELECT 1 FROM planes) THEN
+    INSERT INTO planes (seats, name)
+    VALUES (6, 'SyberJet SJ30i'), (4, 'Cirrus SF50 A'), (4, 'Cirrus SF50 B'), (5, 'HondaJet Elite A'), (5, 'HondaJet Elite B');
+  END IF;
+
+  IF NOT EXISTS (SELECT 1 FROM flights) THEN
+    INSERT INTO flights (plane_id)
+    VALUES (1), (2), (3), (4), (5);
+  END IF;
+
+  IF NOT EXISTS (SELECT 1 FROM routes) THEN
+    INSERT INTO routes(flight_id, departure_location, arrival_location, departure_time, arrival_time, price)
+    VALUES (1, 'NZNE', 'NZRO', '2022-06-01 10:30:00+12', '2022-06-01 11:00:00+12', 5000),
+      (1, 'NZRO', 'YSSY', '2022-06-01 11:15:00+12', '2022-06-01 14:30:00+12', 15000);
+  END IF;
+END;
+$do$
