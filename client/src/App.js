2fa5d5da6b91758c8ef8583f87d8f62b8d8210a6
Vailorium <casterthe5587@gmail.com>
2022-06-22 20:17:59 +1200

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Navigation from './components/common/Navigation';
 import { Routes, Route } from 'react-router-dom';
 import AvailableFlights from './components/AvailableFlights/AvailableFlights';
 import MyBookings from './components/MyBookings/MyBookings';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="flights" element={<AvailableFlights />} />
         <Route path="bookings" element={<MyBookings />} />
       </Routes>
